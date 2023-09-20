64cc96447cfe37b6619138b380a65a5dfa7fe4eb
Agilabrs <arbiansyah.agil11@gmail.com>
2023-09-20 14:32:55 +0700

    tambahan1

--- cobaGudang.java
+++ cobaGudang.java
@@ -3,7 +3,7 @@ public class cobaGudang {
     public static void main(String[] args) {
         Scanner yo = new Scanner(System.in);
         String naBarang1, naBarang2, naBarang3, eta1, eta2, eta3;
-        int totStok, stokBarang1 = 10, stokBarang2 = 7, stokBarang3 = 15;
+        int totStok1, totStok2, totStok3, stokBarang1 = 10, stokBarang2 = 7, stokBarang3 = 15;
         int jmlMasuk, jmlKeluar, exp1, exp2, exp3;
 
         System.out.println("Masukkan nama barang : ");
@@ -11,7 +11,11 @@ public class cobaGudang {
         System.out.println(naBarang1);
         System.out.println("Masukkan tambahan barang : ");
         jmlMasuk = yo.nextInt();
-        totStok = stokBarang1 + jmlMasuk;
-        System.out.println("Stok saat ini : "+ totStok);
+        totStok1 = stokBarang1 + jmlMasuk;
+        System.out.println("Stok saat ini : "+ totStok1);
+        System.out.println("Jumlah barang yang akan anda ambil ");
+        jmlKeluar = yo.nextInt();
+        stokBarang1 = totStok1 - jmlKeluar;
+        System.out.println("jumlah barang saat ini :"+ stokBarang1);
     }
 }
