f96648a3a6391f52218b35b6938e5bbb668adeac
pateh27 <fathzfl@gmail.com>
2023-09-27 11:12:35 +0700

    Update cobaGudang.java

--- cobaGudang.java
+++ cobaGudang.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.time.LocalDate;
 public class cobaGudang {
     public static void main(String[] args) {
         Scanner yo = new Scanner(System.in);
         String naBarang1, naBarang2, naBarang3, eta1, eta2, eta3;
         int totStok1, totStok2, totStok3, stokBarang1 = 10, stokBarang2 = 7, stokBarang3 = 15;
-        int jmlMasuk, jmlKeluar, exp1, exp2, exp3;
+        int jmlMasuk, jmlKeluar;
+        String exp1L, exp2L, exp3L, exp1B, exp2B, exp3B;
 
         System.out.println("Masukkan nama barang ke 1: ");
         naBarang1 = yo.nextLine();
@@ -16,9 +18,11 @@ public class cobaGudang {
         jmlKeluar = yo.nextInt();
         stokBarang1 = totStok1 - jmlKeluar;
         System.out.println("jumlah barang saat ini :"+ stokBarang1);
+        System.out.println("Masukkan tanggal kadaluarsa");
+        exp1L = yo.next();
         
         System.out.println("Masukkan nama barang ke 2: ");
-        naBarang2 = yo.nextLine();
+        naBarang2 = yo.next();
         System.out.println("Masukkan tambahan barang : ");
         jmlMasuk = yo.nextInt();
         totStok2 = stokBarang2 + jmlMasuk;
@@ -27,9 +31,11 @@ public class cobaGudang {
         jmlKeluar = yo.nextInt();
         stokBarang2 = totStok2 - jmlKeluar;
         System.out.println("jumlah barang saat ini :"+ stokBarang2);
+        System.out.println("Masukkan tanggal kadaluarsa");
+        exp2L = yo.next();
         
         System.out.println("Masukkan nama barang ke 3: ");
-        naBarang3 = yo.nextLine();
+        naBarang3 = yo.next();
         System.out.println("Masukkan tambahan barang : ");
         jmlMasuk = yo.nextInt();
         totStok3 = stokBarang3 + jmlMasuk;
@@ -38,5 +44,8 @@ public class cobaGudang {
         jmlKeluar = yo.nextInt();
         stokBarang3 = totStok3 - jmlKeluar;
         System.out.println("jumlah barang saat ini :"+ stokBarang3);
+        System.out.println("Masukkan tanggal kadaluarsa");
+
+        
     }
 }
